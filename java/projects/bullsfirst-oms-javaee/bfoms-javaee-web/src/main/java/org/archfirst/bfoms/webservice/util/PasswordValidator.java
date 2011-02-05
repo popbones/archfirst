24204d624ab77863bab8deeacbf809b2d13c1194
naresh.archfirst@gmail.com <naresh.archfirst@gmail.com@5f1d654b-2d44-f8f1-813d-ae2f855fe689>
2011-02-05 18:08:32 +0000

    Modified PasswordValidator to access the database using JDBC.
    git-svn-id: http://archfirst.googlecode.com/svn/trunk@43 5f1d654b-2d44-f8f1-813d-ae2f855fe689

--- java/projects/bullsfirst-oms-javaee/bfoms-javaee-web/src/main/java/org/archfirst/bfoms/webservice/util/PasswordValidator.java
+++ java/projects/bullsfirst-oms-javaee/bfoms-javaee-web/src/main/java/org/archfirst/bfoms/webservice/util/PasswordValidator.java
@@ -1,20 +1,72 @@
 package org.archfirst.bfoms.webservice.util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.archfirst.bfoms.domain.security.PasswordHashGenerator;
+
 import com.sun.xml.wss.impl.callback.PasswordValidationCallback;
 import com.sun.xml.wss.impl.callback.PasswordValidationCallback.PasswordValidationException;
 import com.sun.xml.wss.impl.callback.PasswordValidationCallback.Request;
 
 public class PasswordValidator implements PasswordValidationCallback.PasswordValidator
 {
-//    @Inject SecurityService securityService;
+    private static String CONNECTION_POOL_JNDI_NAME =
+        "bfoms_javaee_connection_pool";
     
+    private static String READ_PASSWORD =
+        "select passwordHash from Users where username = ?";
+    
+    private static String DB_ERROR = "Error accessing database";
+
     @Override
     public boolean validate(Request request) throws PasswordValidationException {
-//        PasswordValidationCallback.PlainTextPasswordRequest passwordRequest
-//            = (PasswordValidationCallback.PlainTextPasswordRequest)request;
-//        String username = passwordRequest.getUsername();
-//        String password = passwordRequest.getPassword();
-//        return securityService.authenticateUser(username, password).isSuccess();
-        return true;
+
+        // Get the username and password supplied by the user
+        PasswordValidationCallback.PlainTextPasswordRequest passwordRequest
+            = (PasswordValidationCallback.PlainTextPasswordRequest)request;
+        String username = passwordRequest.getUsername();
+        String password = passwordRequest.getPassword();
+        String passwordHash =
+            PasswordHashGenerator.generateSaltedHash(password, username);
+
+        // Get the password hash stored in the database
+        String passwordHashDb = null;
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            Context context = new InitialContext();
+            DataSource datasource =
+                (DataSource)context.lookup(CONNECTION_POOL_JNDI_NAME);
+            connection = datasource.getConnection();
+            statement = connection.prepareStatement(READ_PASSWORD);
+            statement.setString(1, username);
+            resultSet = statement.executeQuery();
+            // ResultSet should have 0 or 1 record
+            while(resultSet.next()) {
+                passwordHashDb = resultSet.getString("passwordHash");
+            }
+        }
+        catch (NamingException e) {
+            throw new PasswordValidationException(DB_ERROR);
+        }
+        catch (SQLException e) {
+            throw new PasswordValidationException(DB_ERROR);
+        }
+        finally {
+            if (resultSet != null) try { resultSet.close(); } catch (SQLException e) {}
+            if (statement != null) try { statement.close(); } catch (SQLException e) {}
+            if (connection != null) try { connection.close(); } catch (SQLException e) {}
+        }
+        
+        return passwordHash.equals(passwordHashDb);
     }
 }
