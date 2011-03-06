ec54df9af5ae152752d1fb7f8724e471c646c8f7
naresh.archfirst@gmail.com <naresh.archfirst@gmail.com@5f1d654b-2d44-f8f1-813d-ae2f855fe689>
2011-03-06 06:16:22 +0000

    Added initial implementation of PositionsView.
    git-svn-id: http://archfirst.googlecode.com/svn/trunk@103 5f1d654b-2d44-f8f1-813d-ae2f855fe689

---
+++ dotnet/Projects/Bullsfirst/Silverlight/Archfirst.Framework/SilverlightHelpers/DecimalToPercentageConverter.cs
@@ -0,0 +1,44 @@
+﻿/* Copyright 2011 Archfirst
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Archfirst.Framework.SilverlightHelpers
+{
+    public class DecimalToPercentageConverter : IValueConverter
+    {
+        /// <summary>
+        /// Used to modify data as it is bound from the source object to the control.
+        /// Converts decimal to percentage string
+        /// </summary>
+        public object Convert(
+            object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return String.Format("{0:0.00}%", ((decimal)value) * 100);
+        }
+
+        /// <summary>
+        /// Used to modify data as it is saved from the control to the source object.
+        /// Converts percentage string to decimal
+        /// But this case never happens, so throw exception
+        /// </summary>
+        public object ConvertBack(
+            object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
