719887873f1061d175e336ad0c5a8af75230225a
naresh.archfirst@gmail.com <naresh.archfirst@gmail.com@5f1d654b-2d44-f8f1-813d-ae2f855fe689>
2011-03-29 19:23:39 +0000

    Added glassfish JMS configuration
    git-svn-id: http://archfirst.googlecode.com/svn/trunk@148 5f1d654b-2d44-f8f1-813d-ae2f855fe689

---
+++ java/examples/jms-perf/simple-producer-glassfish.bat
@@ -0,0 +1,9 @@
+@echo off
+java -classpath ^
+target/jms-perf-1.0.jar;^
+%GLASSFISH_HOME%/modules/gf-client-module.jar;^
+%GLASSFISH_HOME%/modules/javax.jms.jar;^
+%M2_REPO%/log4j/log4j/1.2.16/log4j-1.2.16.jar;^
+%M2_REPO%/org/slf4j/slf4j-api/1.6.1/slf4j-api-1.6.1.jar;^
+%M2_REPO%/org/slf4j/slf4j-log4j12/1.6.1/slf4j-log4j12-1.6.1.jar;^
+    org.archfirst.jmsproducer.SimpleProducer config/jndi-glassfish.properties config/simple-client-glassfish.properties
