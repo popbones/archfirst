7d2ad8d84a1b54219d6e9a071155293908050a52
naresh.archfirst@gmail.com <naresh.archfirst@gmail.com@5f1d654b-2d44-f8f1-813d-ae2f855fe689>
2011-04-27 06:20:21 +0000

    Styles StatusBar.
    git-svn-id: http://archfirst.googlecode.com/svn/trunk@220 5f1d654b-2d44-f8f1-813d-ae2f855fe689

---
+++ dotnet/Projects/Bullsfirst/Silverlight/Bullsfirst.Infrastructure/Controls/CategoryToBrushConverter.cs
@@ -0,0 +1,78 @@
+﻿/* Copyright 2011 Archfirst
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Data;
+using System.Globalization;
+using Microsoft.Practices.Prism.Logging;
+
+namespace Bullsfirst.Infrastructure.Controls
+{
+    public class CategoryToBrushConverter : IValueConverter
+    {
+        private static SolidColorBrush brushBlack = new SolidColorBrush(Colors.Black);
+        private static SolidColorBrush brushGreen = new SolidColorBrush(Colors.Green);
+        private static SolidColorBrush brushBrown = new SolidColorBrush(Colors.Brown);
+        private static SolidColorBrush brushRed = new SolidColorBrush(Colors.Red);
+
+        /// <summary>
+        /// Used to modify data as it is bound from the source object to the control.
+        /// Converts Category to a Brush
+        /// </summary>
+        public object Convert(
+            object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Brush brush = null; ;
+
+            Category category = (Category)value;
+            switch (category)
+            {
+                case Category.Debug:
+                    brush = brushBlack;
+                    break;
+                case Category.Info:
+                    brush = brushGreen;
+                    break;
+                case Category.Warn:
+                    brush = brushBrown;
+                    break;
+                case Category.Exception:
+                    brush = brushRed;
+                    break;
+            }
+
+            return brush;
+        }
+
+        /// <summary>
+        /// Used to modify data as it is saved from the control to the source object.
+        /// Converts Brush to a Category
+        /// But this case never happens, so throw exception
+        /// </summary>
+        public object ConvertBack(
+            object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- dotnet/Projects/Bullsfirst/Silverlight/Bullsfirst.Infrastructure/Controls/StatusBarViewModel.cs
+++ dotnet/Projects/Bullsfirst/Silverlight/Bullsfirst.Infrastructure/Controls/StatusBarViewModel.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Practices.Prism.Logging;
-/* Copyright 2011 Archfirst
+﻿/* Copyright 2011 Archfirst
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +13,8 @@
  * limitations under the License.
  */
 using System.ComponentModel.Composition;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Logging;
 using Microsoft.Practices.Prism.ViewModel;
 
 namespace Bullsfirst.Infrastructure.Controls
@@ -22,15 +23,42 @@ namespace Bullsfirst.Infrastructure.Controls
     [PartCreationPolicy(CreationPolicy.Shared)]
     public class StatusBarViewModel : NotificationObject, IStatusBar
     {
+        #region Construction
+
+        public StatusBarViewModel()
+        {
+            ClearCommand = new DelegateCommand<object>(this.ClearCommandExecute);
+        }
+
+        #endregion
+
+        #region IStatusBar Implementation
+
         public void ShowMessage(string message, Category category, Priority priority)
         {
             StatusMessage = string.Format("[{0}] {1}", category, message);
+            Category = category;
         }
 
         public void Clear()
         {
             StatusMessage = null;
         }
+
+        #endregion
+
+        #region ClearCommand
+
+        public DelegateCommand<object> ClearCommand { get; set; }
+
+        private void ClearCommandExecute(object dummyObject)
+        {
+            this.Clear();
+        }
+
+        #endregion
+
+        #region Members
 
         private string _statusMessage;
         public string StatusMessage
@@ -42,5 +70,18 @@ namespace Bullsfirst.Infrastructure.Controls
                 this.RaisePropertyChanged("StatusMessage");
             }
         }
+
+        private Category _category;
+        public Category Category
+        {
+            get { return _category; }
+            set
+            {
+                _category = value;
+                this.RaisePropertyChanged("Category");
+            }
+        }
+
+        #endregion
     }
 }
