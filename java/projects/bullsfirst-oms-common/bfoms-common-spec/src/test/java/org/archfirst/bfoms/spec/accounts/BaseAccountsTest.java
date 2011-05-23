6a85037c37f692178f32ff2568a22de9212fff1a
naresh.archfirst@gmail.com <naresh.archfirst@gmail.com@5f1d654b-2d44-f8f1-813d-ae2f855fe689>
2011-05-23 14:05:13 +0000

    Started implementing more exhaustive transfer tests.
    git-svn-id: http://archfirst.googlecode.com/svn/trunk@233 5f1d654b-2d44-f8f1-813d-ae2f855fe689

--- java/projects/bullsfirst-oms-common/bfoms-common-spec/src/test/java/org/archfirst/bfoms/spec/accounts/BaseAccountsTest.java
+++ java/projects/bullsfirst-oms-common/bfoms-common-spec/src/test/java/org/archfirst/bfoms/spec/accounts/BaseAccountsTest.java
@@ -15,15 +15,21 @@
  */
 package org.archfirst.bfoms.spec.accounts;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.archfirst.bfoms.domain.account.BaseAccountService;
+import org.archfirst.bfoms.domain.account.brokerage.BrokerageAccount;
 import org.archfirst.bfoms.domain.account.brokerage.BrokerageAccountService;
 import org.archfirst.bfoms.domain.account.external.ExternalAccountParams;
 import org.archfirst.bfoms.domain.account.external.ExternalAccountService;
 import org.archfirst.bfoms.domain.marketdata.MarketDataService;
 import org.archfirst.bfoms.domain.security.RegistrationRequest;
 import org.archfirst.bfoms.domain.security.SecurityService;
+import org.archfirst.common.money.Money;
 import org.archfirst.common.springtest.AbstractTransactionalSpecTest;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -42,8 +48,8 @@ public abstract class BaseAccountsTest extends AbstractTransactionalSpecTest  {
     protected static String BROKERAGE_ACCOUNT_NAME1 = "Brokerage Account 1";
     protected static String BROKERAGE_ACCOUNT_NAME2 = "Brokerage Account 2";
     protected static String EXTERNAL_ACCOUNT_NAME1 = "External Account 1";
-    protected static String EXTERNAL_ROUTING_NUMBER1 = "011000123";
-    protected static String EXTERNAL_ACCOUNT_NUMBER1 = "0157-8965-2278";
+    protected static String EXTERNAL_ROUTING_NUMBER = "011000123";
+    protected static String EXTERNAL_ACCOUNT_NUMBER = "0157-8965-2278";
     
     @Inject protected BaseAccountService baseAccountService;
     @Inject protected BrokerageAccountService brokerageAccountService;
@@ -55,27 +61,90 @@ public abstract class BaseAccountsTest extends AbstractTransactionalSpecTest  {
     protected Long brokerageAccount2Id;
     protected Long externalAccount1Id;
     
+    // Map of account name to account id
+    private Map<String, Long> accountMap = new HashMap<String, Long>(); 
+    
+    protected Long getAccountId(String accountName) {
+        return accountMap.get(accountName);
+    }
+    
     protected void createUser1() throws Exception {
         securityService.registerUser(
                 new RegistrationRequest(FIRST_NAME1, LAST_NAME1, USERNAME1, PASSWORD1));
     }
 
+    protected void createExternalAccount1() {
+        externalAccount1Id = createExternalAccount(EXTERNAL_ACCOUNT_NAME1);
+    }
+    
     protected void createBrokerageAccount1() {
-        brokerageAccount1Id = brokerageAccountService.openNewAccount(
-                USERNAME1, BROKERAGE_ACCOUNT_NAME1);
+        brokerageAccount1Id = createBrokerageAccount(BROKERAGE_ACCOUNT_NAME1);
     }
 
     protected void createBrokerageAccount2() {
-        brokerageAccount2Id = brokerageAccountService.openNewAccount(
-                USERNAME1, BROKERAGE_ACCOUNT_NAME2);
+        brokerageAccount2Id = createBrokerageAccount(BROKERAGE_ACCOUNT_NAME2);
     }
+    
+    private Long createExternalAccount(String accountName) {
 
-    protected void createExternalAccount1() {
-        ExternalAccountParams params = new ExternalAccountParams(
-                EXTERNAL_ACCOUNT_NAME1,
-                EXTERNAL_ROUTING_NUMBER1,
-                EXTERNAL_ACCOUNT_NUMBER1);
-        externalAccount1Id = externalAccountService.addExternalAccount(
-                USERNAME1, params);
+        // Create the account if it does not exist
+        Long accountId = getAccountId(accountName);
+        if (accountId == null) {
+            ExternalAccountParams params = new ExternalAccountParams(
+                    accountName,
+                    EXTERNAL_ROUTING_NUMBER,
+                    EXTERNAL_ACCOUNT_NUMBER);
+            accountId = externalAccountService.addExternalAccount(
+                    USERNAME1, params);
+            accountMap.put(accountName, accountId);
+        }
+        
+        return accountId;
+    }
+    
+    public void setUpExternalAccount(String accountName) {
+        createExternalAccount(accountName);
+    }
+    
+    private Long createBrokerageAccount(String accountName) {
+
+        // Create the account if it does not exist
+        Long accountId = getAccountId(accountName);
+        if (accountId == null) {
+            accountId = brokerageAccountService.openNewAccount(
+                    USERNAME1, accountName);
+            accountMap.put(accountName, accountId);
+        }
+        
+        return accountId;
+    }
+    
+    public void setUpBrokerageAccount(String accountName, BigDecimal cashPosition) {
+        
+        createBrokerageAccount(accountName);
+        
+        // Adjust cash position
+        BigDecimal currentCashPosition = getCashPosition(accountName).getAmount();
+        if (currentCashPosition != cashPosition) {
+            transferCash(
+                    EXTERNAL_ACCOUNT_NAME1,
+                    accountName,
+                    cashPosition.subtract(currentCashPosition));
+        }
+    }
+    
+    public Money getCashPosition(String accountName) {
+        Long accountId = getAccountId(accountName);
+        BrokerageAccount account =
+            brokerageAccountService.findAccount(accountId);
+        return account.getCashPosition();
+    }
+    
+    public void transferCash(String accountName1, String accountName2, BigDecimal amount) {
+        this.baseAccountService.transferCash(
+                USERNAME1,
+                new Money(amount),
+                getAccountId(accountName1),
+                getAccountId(accountName2));
     }
 }
