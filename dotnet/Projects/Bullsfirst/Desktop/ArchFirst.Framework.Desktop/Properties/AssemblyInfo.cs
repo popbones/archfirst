b06653e561f5750285e35e94f2d94ce346a80811
arjun.chakraborty@gmail.com <arjun.chakraborty@gmail.com@5f1d654b-2d44-f8f1-813d-ae2f855fe689>
2011-08-08 04:36:24 +0000

    first drop for WPF Code
    git-svn-id: http://archfirst.googlecode.com/svn/trunk@255 5f1d654b-2d44-f8f1-813d-ae2f855fe689

---
+++ dotnet/Projects/Bullsfirst/Desktop/ArchFirst.Framework.Desktop/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ArchFirst.Framework.Desktop")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Fidelity Investments")]
+[assembly: AssemblyProduct("ArchFirst.Framework.Desktop")]
+[assembly: AssemblyCopyright("Copyright © Fidelity Investments 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("790f5d65-e1c7-472e-8ddc-4aee1f243f6f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ dotnet/Projects/Bullsfirst/Desktop/ArchFirst.Framework.Desktop/WPFHelpers/PasswordChangedBehavior.cs
@@ -0,0 +1,113 @@
+﻿/* Copyright 2011 Archfirst
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Archfirst.Framework.Desktop.WPFHelpers
+{
+    public class PasswordChangedBehavior: Behavior<PasswordBox>
+    {
+        public static readonly DependencyProperty PasswordChangedCommandProperty = DependencyProperty.RegisterAttached("PasswordChangedCommand",
+            typeof(ICommand), 
+            typeof(PasswordChangedBehavior), 
+            new PropertyMetadata(null));
+
+        public static ICommand GetPasswordChangedCommand(DependencyObject target)
+        {
+            return (ICommand)target.GetValue(PasswordChangedCommandProperty);
+        }
+
+        public static void SetPasswordChangedCommand(DependencyObject target,ICommand newValue)
+        {
+            target.SetValue(PasswordChangedCommandProperty,newValue);
+        }
+
+        public static readonly DependencyProperty PasswordHasValidationErrorProperty = DependencyProperty.RegisterAttached("PasswordHasValidationError",
+            typeof(bool),
+            typeof(PasswordChangedBehavior),
+            new PropertyMetadata(PasswordValidationStateChanged));
+
+        public static bool GetPasswordHasValidationError(DependencyObject target)
+        {
+            return (bool)target.GetValue(PasswordHasValidationErrorProperty);
+        }
+
+        public static void SetPasswordHasValidationError(DependencyObject target,bool newValue)
+        {
+            target.SetValue(PasswordHasValidationErrorProperty,newValue);
+        }
+
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PasswordChanged += PasswordBoxPasswordChanged;
+
+            AssociatedObject.GotKeyboardFocus += new KeyboardFocusChangedEventHandler(AssociatedObject_GotKeyboardFocus);
+        }
+
+        void AssociatedObject_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            SetPasswordHasValidationError(AssociatedObject, true);
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.PasswordChanged -= PasswordBoxPasswordChanged;
+            
+        }
+
+        private void PasswordBoxPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(AssociatedObject.Password))
+            {
+                AssociatedObject.SetValue(PasswordHasValidationErrorProperty, true);
+            }
+            else
+            {
+                AssociatedObject.SetValue(PasswordHasValidationErrorProperty, false);
+
+                var command = (ICommand)AssociatedObject.GetValue(PasswordChangedCommandProperty);
+
+                if (command != null)
+                    command.Execute(AssociatedObject.Password);
+            }
+        }
+
+
+        private static void PasswordValidationStateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            PasswordBox pBox = d as PasswordBox;
+
+            if ((bool)e.NewValue == true)
+            {
+                Validation.MarkInvalid(pBox.GetBindingExpression(PasswordChangedCommandProperty), new ValidationError(new ExceptionValidationRule(), pBox.GetBindingExpression(PasswordChangedCommandProperty), "Please enter password", null));
+            }
+            else
+            {
+                Validation.ClearInvalid(pBox.GetBindingExpression(PasswordChangedCommandProperty));
+            }
+
+        }
+    }
+}
