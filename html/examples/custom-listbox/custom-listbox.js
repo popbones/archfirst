d2cf11d168af70f303133312f42d6aaca9e8af4d
naresh.archfirst@gmail.com <naresh.archfirst@gmail.com@5f1d654b-2d44-f8f1-813d-ae2f855fe689>
2011-09-17 22:55:20 +0000

    Removed code for backbone version of listbox and put it in the library.
    git-svn-id: http://archfirst.googlecode.com/svn/trunk@321 5f1d654b-2d44-f8f1-813d-ae2f855fe689

--- html/examples/custom-listbox/custom-listbox.js
+++ html/examples/custom-listbox/custom-listbox.js
@@ -1,31 +1,26 @@
 ﻿/**
-* Copyright 2011 Archfirst
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2011 Archfirst
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 /**
-* @fileoverview Shows multiple ways to build custom ListBoxes.
-*
-* @requires: ../../libs/archfirst/utility/string.supplant.js
-* @requires: ../../libs/archfirst/jquery-archfirst-listbox/jquery.archfirst.listbox.js
-*
-* @author Naresh Bhatia
-*/
+ * @author Naresh Bhatia
+ */
 
-var CustomListBoxExample = window.CustomListBoxExample || {};
+var CustomListboxExample = window.CustomListboxExample || {};
 
-CustomListBoxExample.Run = function () {
+CustomListboxExample.Run = function () {
 
     var fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Fig', 'Grape', 'Kiwi', 'Mango', 'Orange', 'Peach', 'Strawberry'];
     var veggies = [
@@ -45,7 +40,7 @@ CustomListBoxExample.Run = function () {
     function displayItem(itemName) {
         $('#selected-item-display').text(itemName);
     }
-
+    displayItem(fruits[0]);
 
     // ------------------------------------------------------------------------
     // jQuery ListBox
@@ -54,116 +49,14 @@ CustomListBoxExample.Run = function () {
         data: fruits,
         onClickOfItem: displayItem
     });
-    displayItem(fruits[0]);
-
 
     // ------------------------------------------------------------------------
     // Backbone.js ListBox
     // ------------------------------------------------------------------------
-    // Item
-    // ----
-    var Item = Backbone.Model.extend({
-
-        defaults: {
-            value: "undefined",
-            selected: false
-        },
-
-        // Toggle the selected state of this Item.
-        toggleSelected: function () {
-            this.set({ "selected": !this.get("selected") });
-        }
-    });
-
-    // ItemList
-    // --------
-    var ItemList = Backbone.Collection.extend({
-        model: Item,
-
-        findSelectedItem: function () {
-            return this.find(function (item) {
-                return (item.get("selected") == true);
-            });
-        },
-
-        findItemWithValue: function (value) {
-            return this.find(function (item) {
-                return (item.get("value") == value);
-            });
-        }
-    });
-
-    // ItemView
-    // --------
-    var ItemView = Backbone.View.extend({
-
-        tagName: "li",
-
-        initialize: function () {
-            this.model.bind("change:selected", this.renderSelection, this);
-        },
-
-        // Render the contents of the item
-        render: function () {
-            $(this.el).html(this.options.templateString.supplant(this.model.toJSON()));
-            this.renderSelection();
-            return this;
-        },
-
-        renderSelection: function () {
-            if (this.model.get("selected")) {
-                $(this.el).addClass("active");
-            }
-            else {
-                $(this.el).removeClass("active");
-            }
-        }
-    });
-
-    // ItemListView
-    // ------------
-    var ItemListView = Backbone.View.extend({
-
-        events: {
-            "click": "changeSelection"
-        },
-
-        initialize: function () {
-            // Create item views
-            this.collection.each(this.addItemView, this);
-
-            // Select the first one
-            this.collection.at(0).toggleSelected();
-        },
-
-        addItemView: function (item) {
-            var itemView = new ItemView({ model: item, templateString: this.options.templateString });
-            this.el.append(itemView.render().el);
-        },
-
-        changeSelection: function (e) {
-            // TODO: use some other strategy to detect target
-            if ($(e.target).is('a')) {
-                var currentlySelectedItem = this.collection.findSelectedItem();
-                var newlySelectedItem = this.collection.findItemWithValue(e.target.text);
-                if (currentlySelectedItem == null) {
-                    newlySelectedItem.toggleSelected();
-                }
-                else if (currentlySelectedItem != newlySelectedItem) {
-                    currentlySelectedItem.toggleSelected();
-                    newlySelectedItem.toggleSelected();
-                }
-                // Call the selection callback supplied by user
-                this.options.selectionCallback(newlySelectedItem.get("value"));
-            }
-        }
-    });
-
-    var veggieList = new ItemList(veggies);
-    var veggieListView = new ItemListView({
+    var veggieListView = new BackboneArchfirstListbox.ItemListView({
         el: $("#veggieList"),
         templateString: "<a href='#'>{value}</a>",
-        collection: veggieList,
+        collection: new BackboneArchfirstListbox.ItemList(veggies),
         selectionCallback: displayItem
     });
 }
