9451b89b63887294b9556b5046459b8da9952b0d
naresh.archfirst@gmail.com <naresh.archfirst@gmail.com@5f1d654b-2d44-f8f1-813d-ae2f855fe689>
2011-09-23 04:17:01 +0000

    Added REST prototype and example.
    git-svn-id: http://archfirst.googlecode.com/svn/trunk@332 5f1d654b-2d44-f8f1-813d-ae2f855fe689

---
+++ java/examples/jaxrs-oms/src/main/java/org/archfirst/jaxrsoms/Order.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2011 Archfirst
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.archfirst.jaxrsoms;
+
+/**
+ * Order
+ *
+ * @author Naresh Bhatia
+ */
+public class Order {
+    
+    // ----- Constructors -----
+    public Order() {
+    }
+
+    public Order(int id, String side, String symbol, int quantity) {
+        this.id = id;
+        this.side = side;
+        this.symbol = symbol;
+        this.quantity = quantity;
+    }
+
+    // ----- Attributes -----
+    public int id;
+    public String side;
+    public String symbol;
+    public int quantity;
+
+    // ----- Getters and Setters -----
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSide() {
+        return side;
+    }
+    public void setSide(String side) {
+        this.side = side;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
