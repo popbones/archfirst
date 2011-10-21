9b0d81e49a61b139373e621d72a7a1ac8e02d844
naresh.archfirst@gmail.com <naresh.archfirst@gmail.com@5f1d654b-2d44-f8f1-813d-ae2f855fe689>
2011-10-21 03:12:18 +0000

    Added html version of bullsfirst.
    git-svn-id: http://archfirst.googlecode.com/svn/trunk@381 5f1d654b-2d44-f8f1-813d-ae2f855fe689

---
+++ html/apps/bullsfirst/js/script.js
@@ -0,0 +1,65 @@
+/**
+* Copyright 2011 Archfirst
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/**
+* @author Naresh Bhatia
+*/
+
+var Bullsfirst = window.Bullsfirst || {};
+
+Bullsfirst.ready = function () {
+
+    var username, password, user;
+
+    $('#loginForm').submit(function () {
+        username = $('#loginForm #username').val();
+        password = $('#loginForm #password').val();
+        login();
+        return false;
+    });
+
+    /**
+     * Logs in to the server using saved credentials. If login is successful,
+     * saves the returned user information in the user object.
+     */
+    function login() {
+        $.ajax({
+            url: '/bfoms-javaee/rest/secure/users/' + username,
+            beforeSend: setAuthorizationHeader,
+            success: function (data, textStatus, jqXHR) {
+                user = data;
+                $('#message').html('Welcome ' + user.firstName + ' ' + user.lastName + '!');
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $('#message').html(textStatus + ': ' + errorThrown);
+            }
+        });
+    }
+
+    /**
+     * Sets an Authorization header in the request. We force this header in every
+     * request to avoid being challenged by the server for credentials (the server
+     * sends a 401 Unauthorized error along with a WWW-Authenticate header to do this).
+     * Specifically, we don't rely on username/password settings in the jQuery.ajax()
+     * call since they cause an unnecessary roundtrip to the server resulting in a 401
+     * before sending the Authorization header.
+     */
+    function setAuthorizationHeader(xhr) {
+        xhr.setRequestHeader(
+            'Authorization',
+            'Basic ' + base64_encode(username + ':' + password));
+    }
+}
