072c922557774e6c1b7ccfe3cef01eccda83f2f4
choapong@gmail.com <choapong@gmail.com@5f1d654b-2d44-f8f1-813d-ae2f855fe689>
2012-02-14 18:04:22 +0000

    added next and previous
    git-svn-id: http://archfirst.googlecode.com/svn/trunk@530 5f1d654b-2d44-f8f1-813d-ae2f855fe689

--- ios/Projects/Bullsfirst/Bullsfirst/TradeViewController.h
+++ ios/Projects/Bullsfirst/Bullsfirst/TradeViewController.h
@@ -13,8 +13,12 @@
     BFPosition *position;
 }
 
+@property (nonatomic, retain) UITextField *activeTextField;
+@property (nonatomic, retain) NSArray *textFields;
 @property (nonatomic, retain) BFPosition *position;
 @property (strong, nonatomic) IBOutlet UITextField *cusipText;
+@property (strong, nonatomic) IBOutlet UITextField *quantity;
+@property (strong, nonatomic) IBOutlet UITextField *limit;
 
 - (IBAction)cancelBTNClicked:(id)sender;
 - (IBAction)okBTNClicked:(id)sender;
--- ios/Projects/Bullsfirst/Bullsfirst/TradeViewController.m
+++ ios/Projects/Bullsfirst/Bullsfirst/TradeViewController.m
@@ -11,6 +11,10 @@
 @implementation TradeViewController
 @synthesize position;
 @synthesize cusipText;
+@synthesize quantity;
+@synthesize limit;
+@synthesize activeTextField;
+@synthesize textFields;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil position:(BFPosition *)aPosition
 {
@@ -31,10 +35,13 @@
 
 #pragma mark - View lifecycle
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    textFields = [NSArray arrayWithObjects:cusipText, quantity, limit, nil];
+
     if (self.position != nil) {
         cusipText.text = self.position.instrumentSymbol;
     }
@@ -43,6 +50,8 @@
 - (void)viewDidUnload
 {
     [self setCusipText:nil];
+    [self setQuantity:nil];
+    [self setLimit:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -56,9 +65,56 @@
 
 - (IBAction)cancelBTNClicked:(id)sender {
     [self dismissModalViewControllerAnimated:YES];
+    [activeTextField resignFirstResponder];
 }
 
 - (IBAction)okBTNClicked:(id)sender {
     [self dismissModalViewControllerAnimated:YES];
+    [activeTextField resignFirstResponder];
 }
+
+#pragma mark - text field lifecycle
+- (void)previousBTNClicked:(id)sender {
+    NSUInteger currentTextField = [textFields indexOfObject:activeTextField];
+    if (currentTextField == 0)
+        currentTextField = [textFields count] -1;
+    else
+        currentTextField--;
+    activeTextField = [textFields objectAtIndex:currentTextField];
+    [activeTextField becomeFirstResponder];
+}
+
+- (void)nextBTNClicked:(id)sender {
+    NSUInteger currentTextField = [textFields indexOfObject:activeTextField];
+    if (currentTextField < [textFields count]-1)
+        currentTextField++;
+    else
+        currentTextField = 0;
+    activeTextField = [textFields objectAtIndex:currentTextField];
+    [activeTextField becomeFirstResponder];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self okBTNClicked:nil];
+    return NO;
+}
+
+-(void)textFieldDidBeginEditing:(UITextField *)textField{
+    UIToolbar *toolbar = [[UIToolbar alloc] init];
+    [toolbar setBarStyle:UIBarStyleBlackTranslucent];
+    [toolbar sizeToFit];
+    
+    UIBarButtonItem *previousButton = [[UIBarButtonItem alloc] initWithTitle:@"Prev" style:UIBarButtonItemStyleBordered target:self action:@selector(previousBTNClicked:)];
+    UIBarButtonItem *nextButton = [[UIBarButtonItem alloc] initWithTitle:@"Next" style:UIBarButtonItemStyleBordered target:self action:@selector(nextBTNClicked:)];    
+    NSArray *itemsArray = [NSArray arrayWithObjects:previousButton, nextButton, nil];
+    [toolbar setItems:itemsArray];
+    
+    [textField setInputAccessoryView:toolbar];
+    
+    // Set the active field. We' ll need that if we want to move properly
+    // between our textfields.
+    activeTextField = textField;
+}
+
 @end
