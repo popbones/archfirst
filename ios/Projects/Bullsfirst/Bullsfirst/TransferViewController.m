8ba2e4b3189c323a2f3578ba7df1920049fe686f
vivekanarther <vivekanarther@5f1d654b-2d44-f8f1-813d-ae2f855fe689>
2012-03-02 11:01:47 +0000

    Successful Transfer leads to transactions screen
    git-svn-id: http://archfirst.googlecode.com/svn/trunk@758 5f1d654b-2d44-f8f1-813d-ae2f855fe689

--- ios/Projects/Bullsfirst/Bullsfirst/TransferViewController.m
+++ ios/Projects/Bullsfirst/Bullsfirst/TransferViewController.m
@@ -14,6 +14,7 @@
 #import "BFMoney.h"
 #import "AddExternalAccountViewController.h"
 #import "BFConstants.h"
+#import "AppDelegate.h"
 @implementation TransferViewController
 @synthesize segmentedControl,restServiceObject,symbol,amount,quantity,pricePaid;
 @synthesize fromAccountBTN,toAccountBTN,dropdown,transferBTN;
@@ -248,6 +249,8 @@
     spinner.hidden = YES;
     [self dismissModalViewControllerAnimated:YES];    
     [[NSNotificationCenter defaultCenter] postNotificationName:@"REFRESH_ACCOUNT" object:nil];
+    AppDelegate* appDelegate = (AppDelegate*) [[UIApplication sharedApplication] delegate];
+    appDelegate.tabBarController.selectedIndex = 2;
 
 }
 
@@ -280,6 +283,8 @@
     spinner.hidden = YES;
     [self dismissModalViewControllerAnimated:YES];    
     [[NSNotificationCenter defaultCenter] postNotificationName:@"REFRESH_ACCOUNT" object:nil];
+    AppDelegate* appDelegate = (AppDelegate*) [[UIApplication sharedApplication] delegate];
+    appDelegate.tabBarController.selectedIndex = 2;
 
 }
 -(IBAction)cancelBTNClicked:(id)sender
