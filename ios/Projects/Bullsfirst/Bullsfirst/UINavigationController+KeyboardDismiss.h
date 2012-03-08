ab216eb8047607076ceff9ed1772091d7cd796b2
choapong@gmail.com <choapong@gmail.com@5f1d654b-2d44-f8f1-813d-ae2f855fe689>
2012-03-08 21:02:17 +0000

    added categories to NavigationViewController to support keyboard disappearing.
    git-svn-id: http://archfirst.googlecode.com/svn/trunk@839 5f1d654b-2d44-f8f1-813d-ae2f855fe689

---
+++ ios/Projects/Bullsfirst/Bullsfirst/UINavigationController+KeyboardDismiss.h
@@ -0,0 +1,15 @@
+//
+//  UINavigationController+KeyboardDismiss.h
+//  Bullsfirst
+//
+//  Created by Pong Choa on 3/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UINavigationController (KeyboardDismiss)
+
+- (BOOL)disablesAutomaticKeyboardDismissal;
+
+@end
---
+++ ios/Projects/Bullsfirst/Bullsfirst/UINavigationController+KeyboardDismiss.m
@@ -0,0 +1,18 @@
+//
+//  UINavigationController+KeyboardDismiss.m
+//  Bullsfirst
+//
+//  Created by Pong Choa on 3/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "UINavigationController+KeyboardDismiss.h"
+
+@implementation UINavigationController(KeyboardDismiss)
+
+- (BOOL)disablesAutomaticKeyboardDismissal
+{
+    return NO;
+}
+
+@end
