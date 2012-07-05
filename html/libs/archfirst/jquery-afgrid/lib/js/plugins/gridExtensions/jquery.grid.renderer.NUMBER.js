42d1bed6beec433ea618058519d21e26d8b30653
manish.shanker81@gmail.com <manish.shanker81@gmail.com@5f1d654b-2d44-f8f1-813d-ae2f855fe689>
2012-07-05 17:19:24 +0000

    demo prep
    git-svn-id: http://archfirst.googlecode.com/svn/trunk@960 5f1d654b-2d44-f8f1-813d-ae2f855fe689

---
+++ html/libs/archfirst/jquery-afgrid/lib/js/plugins/gridExtensions/jquery.grid.renderer.NUMBER.js
@@ -0,0 +1,10 @@
+(function ($) {
+
+    $.afGrid = $.afGrid || {};
+    $.afGrid.renderer = $.afGrid.renderer || {};
+
+    $.afGrid.renderer.NUMBER = function (data) {
+        return data;
+    };
+
+}(jQuery));
